c29fa9bdac4eb861a814ee673e3013abde90d578
NatalySheludko <137063086+NatalySheludko@users.noreply.github.com>
2023-08-02 17:16:32 +0300

    add js apiURL and apiKey

---
+++ src/scrypt.js
@@ -0,0 +1,4 @@
+let apiKey = "6fa3cb02fc6ct4bd31ab65905b1ado1a";
+let currentCity = "Zaporizhzhia";
+let unit = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${currentCity}&key=${apiKey}&units=${unit}`;
